77901c8af71c81dba5149e59c2a369c515517b26
X-nimesh <nimush14@gmail.com>
2022-01-09 20:10:19 +0545

    isuueeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
 
-import about from '../Component/Pages/about'
+import About from '../Component/Pages/about'
 import Home from '../Component/Pages/Home'
 import Products from '../Component/Pages/Products'
 import './App.css'
@@ -9,24 +9,24 @@ import './App.css'
 function App() {
   
   return (
+    
     <div className="App">
-      <h1>asd</h1>
-    <Router>
-      <Link href="/home">hey</Link>
-        <Routes>
-          <Route exact path='/'>
-            <Home />
-            <h1>hey2</h1>
-          </Route>
-          <Route path='/about'>
-            <about />
-          </Route>
-       
-        </Routes>
-        
-    </Router>
+    <h1>Welcome to React Router!</h1>
+
+
+    <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Switch>
+
+      {/* <Routes>
+        <Route path='/' element={<Home />} />                 
+        <Route path='/about' element={<About />} />                 
+        <Route path='/products' element={<Products />} />          
+      </Routes> */}
+
     </div>
-  
+    
   )
 }
 
